99655aad2b2dd40936729fff2d6b9d9c804e5d8d
PedroSinhuk <pehsinhuk@gmail.com>
2026-06-12 13:19:13 -0300

    first commit

---
+++ cadastroDeTeste/src/main/java/com/PrimeiroTeste/cadastroDeTeste/CadastroDeTesteApplication.java
@@ -0,0 +1,13 @@
+package com.PrimeiroTeste.cadastroDeTeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeTesteApplication.class, args);
+	}
+
+}
---
+++ cadastroDeTeste/src/test/java/com/PrimeiroTeste/cadastroDeTeste/CadastroDeTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PrimeiroTeste.cadastroDeTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
