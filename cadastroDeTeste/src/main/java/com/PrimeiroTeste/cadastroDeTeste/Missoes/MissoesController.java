0fce20c6a6019d30e76e12cdb779d9226146e3d6
PedroSinhuk <pehsinhuk@gmail.com>
2026-07-15 14:35:39 -0300

    feat: Cria as rotas do Controller missoes

---
+++ cadastroDeTeste/src/main/java/com/PrimeiroTeste/cadastroDeTeste/Missoes/MissoesController.java
@@ -0,0 +1,36 @@
+package com.PrimeiroTeste.cadastroDeTeste.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
+
+public class MissoesController {
+
+    // GET -- Mandar uma requisiçao para mostrar missoes
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Missoes listadas com sucesso";
+    }
+
+    //POST -- Mandar uma requisiçao para criar as missoes
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso";
+    }
+
+    //PUT -- Mandar uma requisao para alterar as missoes
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missao alterada com sucesso";
+    }
+
+    //DELETE -- Mandar uma requisiçao para deletar as missoes
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada";
+        }
+
+    }
+
+}
